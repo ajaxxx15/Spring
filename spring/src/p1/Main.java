df2f8b493b6e895930be09a69632d0e01b964df2
Somee <Somee@somee-pc.mshome.net>
2018-12-22 23:20:22 +0530

    First Commit

---
+++ spring/src/p1/Main.java
@@ -0,0 +1,14 @@
+package p1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("actionContext.xml");
+		Employee e = (Employee) ac.getBean("id1");
+		System.out.println(e.getCompany_name());
+
+	}
+
+}
